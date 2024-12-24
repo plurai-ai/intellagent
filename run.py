04fd0aa7f5cad5b579dcdee14ae76030cf9e536c
Eladlev <elad.levi4@mail.huji.ac.il>
2024-12-24 14:33:39 +0200

    Adding experiments checkpoints Fixing report bugs in case of failure rows

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import argparse
 from simulator.utils.file_reading import override_config
 from simulator.simulator_executor import SimulatorExecutor
 import warnings
+
 warnings.filterwarnings("ignore", category=UserWarning, module="langsmith")
 
 
@@ -10,8 +11,10 @@ def parse_args():
     parser.add_argument('--config_path', type=str, default='config/config_default.yml',
                         help="The configuration diff file path.")
     parser.add_argument('--output_path', type=str, required=True, help="Path to the output file.")
-    parser.add_argument('--dataset',  type=str, default='latest', help="The dataset name. "
-                                                                   "If 'latest', the latest dataset will be loaded.")
+    parser.add_argument('--dataset', type=str, default='latest', help="The dataset name. "
+                                                                      "If 'latest', the latest dataset will be loaded.")
+    parser.add_argument('--experiment', type=str, default='', help="The experiment name. "
+                                                                   "Default (empty) creating a new one")
     return parser.parse_args()
 
 
@@ -23,7 +26,7 @@ def main():
     # Loading the dataset default is latest, if you want to load a specific dataset, pass the path
     executor.load_dataset(args.dataset)
     # Run the simulation on the dataset
-    executor.run_simulation()
+    executor.run_simulation(args.experiment)
     print("Processing complete.")
 
 
